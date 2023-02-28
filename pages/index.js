507e5190653f16c2c1f17fb8f9d9f7e6113a2053
Phu Ngo <ptn123@gmail.com>
2023-02-27 21:05:41 -0600

    init

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div />;
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  )
 }
---
+++ pages/token-topup.js
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Token Topup Page</h1>
+      </div>
+    )
+  }
+  
